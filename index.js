dcd4fdd1c3c837be05ec004625f6e5283bc34f51
nancy sin <nsin@sbcglobal.net>
2018-01-04 12:40:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, Object, {[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]= value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  newObj= object.assign({}, object)
+  delete newObj =[key]
+  return newObj]
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
